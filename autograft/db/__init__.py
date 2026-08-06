700ffcad2506310d4fa925c72b4eb54f941c5db8
Lead Developer <dev@kyrosdata.com>
2026-08-06 10:06:32 +0200

    feat: implement neo4j cypher query generator

--- autograft/db/__init__.py
+++ autograft/db/__init__.py
@@ -1,1 +1,4 @@
-# DB Init
+"""DB package exports."""
+from autograft.db.neo4j_generator import generate_merge_query
+
+__all__ = ["generate_merge_query"]
---
+++ autograft/db/neo4j_generator.py
@@ -0,0 +1,16 @@
+"""Neo4j Cypher query generator for creating or merging entity nodes."""
+from autograft.models.entities import Entity, MatchResult
+
+
+def generate_merge_query(new_entity: Entity, match_result: MatchResult) -> str:
+    """Generates a Cypher query to create or update an entity node in Neo4j."""
+    if not match_result.is_match:
+        return (
+            "MERGE (n:Entity {canonical_name: $name}) "
+            "SET n.type = $type, n.aliases = $aliases RETURN n"
+        )
+
+    return (
+        f"MATCH (n:Entity {{node_id: '{match_result.matched_node_id}'}}) "
+        "SET n.aliases = $aliases RETURN n"
+    )
